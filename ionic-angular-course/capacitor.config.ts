1d641ca07f187845b8ac7efc3f4b231ae1cb9d1e
Caul Pearson <caulpearson@gmail.com>
2022-07-07 13:43:17 -0500

    Android app build

--- ionic-angular-course/capacitor.config.ts
+++ ionic-angular-course/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.IonicAngularCourse',
   appName: 'ionic-angular-course',
   webDir: 'www',
   bundledWebRuntime: false
