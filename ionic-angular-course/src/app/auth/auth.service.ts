ea7c65beb1b1f42e3d7e93bdda7497bee67a6182
Caul Pearson <caulpearson@gmail.com>
2022-07-17 15:05:29 -0500

    pseudologin with auth service

---
+++ ionic-angular-course/src/app/auth/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private userIsAuthenticated = false;
+
+  constructor() { }
+  get getUserIsAuthenticated(){
+      return this.userIsAuthenticated;
+    }
+  login(){
+    this.userIsAuthenticated = true;
+  }
+  logout(){
+    this.userIsAuthenticated = false;
+  }
+}
